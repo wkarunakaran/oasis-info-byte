314ceb223244b69b079d80758c1f9c4eb2abaa65
wkarunakaran <146978759+wkarunakaran@users.noreply.github.com>
2023-10-29 10:15:28 +0530

    Add files via upload

---
+++ iris.ds.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.datasets import load_iris
+
+# Load the Iris dataset from the UCI Machine Learning Repository
+iris = load_iris()
+X = pd.DataFrame(iris.data, columns=iris.feature_names)
+y = pd.Series(iris.target)
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train the K-Nearest Neighbors classifier
+classifier = KNeighborsClassifier(n_neighbors=3)
+classifier.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = classifier.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+confusion = confusion_matrix(y_test, y_pred)
+report = classification_report(y_test, y_pred, target_names=iris.target_names)
+
+# Print evaluation metrics
+print(f'Accuracy: {accuracy}')
+print(f'Confusion Matrix:\n{confusion}')
+print(f'Classification Report:\n{report}')
